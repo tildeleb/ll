091f52f299ffc4e752e671284b6786994b8840ff
Lawrence E. Bakst <leb@me.com>
2019-06-01 22:45:44 -0400

    version with Pair named List

--- lispylist.go
+++ lispylist.go
@@ -23,29 +23,28 @@ import (
 // I would have liked to call this Pair and make a type
 // type List *Pair
 // I can't do that because methods can not have pointer types a receivers.
-type Pair struct {
+type List struct {
 	Head interface{}
 	Tail interface{}
 }
-type List *Pair
 
 // Cons creates a single element of a List
 // Cons(1, nil) creates a single element list
-func Cons(a, b interface{}) List {
+func Cons(a, b interface{}) *List {
 	//fmt.Printf("cons: (%v . %v)\n", a, b)
-	return &Pair{a, b}
+	return &List{a, b}
 }
 
 // Last returns the last element of a list as a list.
-func Last(x List) List {
+func Last(x *List) *List {
 	for x.Tail != nil {
-		x = x.Tail.(List)
+		x = x.Tail.(*List)
 	}
 	return x
 }
 
 // Nconc is like append but it shares structure with the lists that are passed to it.
-func Nconc(x, y List) List {
+func Nconc(x, y *List) *List {
 	switch {
 	case x == nil && y == nil:
 		return nil
@@ -64,14 +63,14 @@ func Nconc(x, y List) List {
 }
 
 // flatten2 takes a nested list and flattens it.
-func Flatten2(tree interface{}) List {
-	var rec func(x interface{}, acc List) List
-	rec = func(x interface{}, acc List) List {
+func Flatten(tree interface{}) *List {
+	var rec func(x interface{}, acc *List) *List
+	rec = func(x interface{}, acc *List) *List {
 		if x == nil {
 			return acc
 		}
 		switch v := x.(type) {
-		case List:
+		case *List:
 			return rec(v.Head, rec(v.Tail, acc))
 		default:
 			if acc == nil {
@@ -84,33 +83,33 @@ func Flatten2(tree interface{}) List {
 }
 
 // flatten takes a nested list and flattens it.
-func Flatten1(tree List) List {
+func FlattenSlow(tree *List) *List {
 	if tree == nil {
 		return nil
 	}
-	Head, Headok := tree.Head.(List)
-	Tail, _ := tree.Tail.(List)
+	Head, Headok := tree.Head.(*List)
+	Tail, _ := tree.Tail.(*List)
 	if Headok {
-		return Nconc(Flatten1(Head), Flatten1(Tail))
+		return Nconc(FlattenSlow(Head), FlattenSlow(Tail))
 	}
-	return Nconc(Cons(tree.Head, nil), Flatten1(Tail))
+	return Nconc(Cons(tree.Head, nil), FlattenSlow(Tail))
 }
 
 // List creates a list of whatever is passed in as rest.
-func MakeList(rest ...interface{}) List {
+func MakeList(rest ...interface{}) *List {
 	h := Cons(rest[0], nil)
 	l := h
 	for _, v := range rest[1:] {
 		l.Tail = Cons(v, nil)
-		l = l.Tail.(List)
+		l = l.Tail.(*List)
 	}
 	return h
 }
 
 // Print prints a list
-func Print(list List) {
-	var p func(list List)
-	p = func(list List) {
+func (list *List) Print() {
+	var p func(list *List)
+	p = func(list *List) {
 		fmt.Printf("(")
 		prev := list
 		for {
@@ -118,7 +117,7 @@ func Print(list List) {
 				break
 			}
 			if prev.Head != nil {
-				h, ok := prev.Head.(List)
+				h, ok := prev.Head.(*List)
 				if ok {
 					p(h)
 				} else {
@@ -131,7 +130,7 @@ func Print(list List) {
 				}
 			}
 			if prev.Tail != nil {
-				prev = prev.Tail.(List)
+				prev = prev.Tail.(*List)
 				fmt.Printf(" ")
 			} else {
 				break
@@ -143,49 +142,6 @@ func Print(list List) {
 	fmt.Printf("\n")
 }
 
-/*
-func main() {
-	lst := genNestedList(1, 20, 5)
-	Print(lst)
-	Print(flatten2(lst))
-	return
-	Print(flatten2(List(1, 2, 3)))
-	Print(genList(1, 3))
-	h := Cons(1, nil)
-	l := h
-	l.Tail = Cons(2, nil)
-	l = l.Tail.(*Pair)
-	l.Tail = Cons(3, nil)
-	l1 := List(1)
-	l2 := List(2)
-	l3 := List(3, 4)
-	l4 := List(5, 6, 7)
-	s := []int{1, 2, 3, 4, 5, 6}
-	l5 := List(s)
-	l6 := List("how", "now", "brown", "cow")
-	l7 := List(1, List(2, 3, List(4, 5, 6)), List(7, 8))
-	l8 := List(List(List(List(1, 2), 3), 4), List(5, 6), List(7, 8, 9), List(10, 11))
-	Print(h)
-	Print(l1)
-	Print(l2)
-	Print(Last(l2))
-	Print(l2)
-	Print(Nconc(l1, l2))
-	Print(l3)
-	Print(l4)
-	Print(Nconc(l3, l4))
-	Print(l5)
-	Print(l6)
-	Print(l7)
-	fmt.Print("14: ")
-	Print(l4)
-	Print(flatten2(l4))
-	fmt.Print("17: ")
-	Print(l7)
-	Print(flatten2(l7))
-	f := flatten2(l8)
-	fmt.Print("18: ")
-	Print(f)
-	Print(nil)
+func Print(list *List) {
+	list.Print()
 }
-*/
--- lispylist_test.go
+++ lispylist_test.go
@@ -15,7 +15,7 @@ func rbetween(a int, b int) int {
 	return r.Intn(b-a+1) + a
 }
 
-func GenList(start, length int) List {
+func GenList(start, length int) *List {
 	idx := start + length - 1
 	l := Cons(idx, nil)
 	idx--
@@ -26,9 +26,9 @@ func GenList(start, length int) List {
 	return l
 }
 
-func GenNestedList(v, n, depth int) List {
-	var gnl func(d int) List
-	gnl = func(d int) List {
+func GenNestedList(v, n, depth int) *List {
+	var gnl func(d int) *List
+	gnl = func(d int) *List {
 		d--
 		if d > 0 {
 			lst := Cons(nil, nil)
@@ -48,11 +48,11 @@ func GenNestedList(v, n, depth int) List {
 	return gnl(d)
 }
 
-func benchmarkFlatten1(b *testing.B, l []List) {
+func benchmarkFlatten(b *testing.B, l []*List) {
 	idx := 0
 	length := len(l)
 	for i := 0; i < b.N; i++ {
-		Flatten1(l[idx])
+		Flatten(l[idx])
 		idx++
 		if idx >= length {
 			idx = 0
@@ -60,11 +60,11 @@ func benchmarkFlatten1(b *testing.B, l []List) {
 	}
 }
 
-func benchmarkFlatten2(b *testing.B, l []List) {
+func benchmarkFlattenSlow(b *testing.B, l []*List) {
 	idx := 0
 	length := len(l)
 	for i := 0; i < b.N; i++ {
-		Flatten2(l[idx])
+		FlattenSlow(l[idx])
 		idx++
 		if idx >= length {
 			idx = 0
@@ -72,19 +72,95 @@ func benchmarkFlatten2(b *testing.B, l []List) {
 	}
 }
 
-func BenchmarkFlatten1(b *testing.B) {
-	benchmarkFlatten1(b, ll)
+func BenchmarkFlatten(b *testing.B) {
+	benchmarkFlatten(b, ll)
 }
 
-func BenchmarkFlatten2(b *testing.B) {
-	benchmarkFlatten2(b, ll)
+func BenchmarkFlattenSlow(b *testing.B) {
+	benchmarkFlattenSlow(b, ll)
 }
 
 var n = 10000
-var ll = make([]List, n)
+var ll = make([]*List, n)
 
 func init() {
 	for i := 0; i < n; i++ {
 		ll[i] = GenNestedList(1, 50, 5)
 	}
 }
+
+func ExampleOfList() {
+	list := MakeList(1, 2, 3, 4, 5)
+	Print(list)
+	// Output:
+	// (1 2 3 4 5)
+}
+
+func ExampleOfFlatten() {
+	list := MakeList(1, MakeList(2, 3, MakeList(4, 5, 6)), MakeList(7, 8))
+	Print(list)
+	Print(Flatten(list))
+	// Output:
+	// (1 (2 3 (4 5 6)) (7 8))
+	// (1 2 3 4 5 6 7 8)
+}
+
+func Example001() {
+	h := Cons(1, nil)
+	Print(h)
+	l := h
+	l.Tail = Cons(2, nil)
+	l = l.Tail.(*List)
+	l.Tail = Cons(3, nil)
+	Print(h)
+	// Output:
+	// (1)
+	// (1 2 3)
+}
+
+/*
+func main() {
+	lst := genNestedList(1, 20, 5)
+	Print(lst)
+	Print(flatten2(lst))
+	return
+	Print(flatten2(List(1, 2, 3)))
+	Print(genList(1, 3))
+	h := Cons(1, nil)
+	l := h
+	l.Tail = Cons(2, nil)
+	l = l.Tail.(*Pair)
+	l.Tail = Cons(3, nil)
+	l1 := List(1)
+	l2 := List(2)
+	l3 := List(3, 4)
+	l4 := List(5, 6, 7)
+	s := []int{1, 2, 3, 4, 5, 6}
+	l5 := List(s)
+	l6 := List("how", "now", "brown", "cow")
+	l7 := List(1, List(2, 3, List(4, 5, 6)), List(7, 8))
+	l8 := List(List(List(List(1, 2), 3), 4), List(5, 6), List(7, 8, 9), List(10, 11))
+	Print(h)
+	Print(l1)
+	Print(l2)
+	Print(Last(l2))
+	Print(l2)
+	Print(Nconc(l1, l2))
+	Print(l3)
+	Print(l4)
+	Print(Nconc(l3, l4))
+	Print(l5)
+	Print(l6)
+	Print(l7)
+	fmt.Print("14: ")
+	Print(l4)
+	Print(flatten2(l4))
+	fmt.Print("17: ")
+	Print(l7)
+	Print(flatten2(l7))
+	f := flatten2(l8)
+	fmt.Print("18: ")
+	Print(f)
+	Print(nil)
+}
+*/
